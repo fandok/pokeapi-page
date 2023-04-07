8c7928f8a4655646f9859ffd7b2cc8d9d53e8e53
fandok <fandika.okdiba@gmail.com>
2023-04-07 15:05:44 +0700

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+import imgLogo from "./assets/images/logo_pokedex.png";
+import imgSearch from "./assets/images/search.png";
+import imgTag from "./assets/images/tag.png";
 
+const App = () => {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <div className="header">
+        <div className="headerMain">
+          <img
+            width={24}
+            height={24}
+            className="headerLogo"
+            src={imgLogo}
+            alt="logo"
+          />
+          <div className="headerTitle">Pokédex</div>
+        </div>
+        <div className="headerSearch">
+          <div className="searchBar">
+            <img
+              className="searchImage"
+              width={16}
+              height={16}
+              src={imgSearch}
+              alt="search"
+            />
+            <input className="searchInput" placeholder="Search" />
+          </div>
+          <div className="searchTag">
+            <img src={imgTag} alt="tag" width={16} height={16} />
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
